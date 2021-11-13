b927e1e2d5358547adf68b952b51d7d3d4749735
Jorge Retamal <jorgeretamalaburto@gmail.com>
2021-11-13 03:06:02 -0300

    router finish

--- angular-project/src/app/features/features-routing.module.ts
+++ angular-project/src/app/features/features-routing.module.ts
@@ -2,10 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
-const routes: Routes = [{ path: '', component: MainLayoutComponent }, { path: 'pokemon', loadChildren: () => import('./pokemon/pokemon.module').then(m => m.PokemonModule) }, { path: 'digimon', loadChildren: () => import('./digimon/digimon.module').then(m => m.DigimonModule) }];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: 'pokemon',
+        loadChildren: () =>
+          import('./pokemon/pokemon.module').then((m) => m.PokemonModule),
+      },
+      {
+        path: 'digimon',
+        loadChildren: () =>
+          import('./digimon/digimon.module').then((m) => m.DigimonModule),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class FeaturesRoutingModule { }
+export class FeaturesRoutingModule {}
